0828abd8f25730bca527a3a36ed6c862d52f3cde
GuilhermeDev1 <deckdark666@gmail.com>
2022-12-29 12:56:47 -0300

    Mister bot

---
+++ README.md
@@ -0,0 +1,43 @@
+<p align="center">
+<img height="250" src="https://user-images.githubusercontent.com/82925878/206579312-1cda7918-c0c8-4e57-bfc4-6102c7b05b0d.gif" align="right">
+
+<h1 align="center">☃️🎀 Jeth 🎀☃️</h1>
+
+<!-- Badges, about the GitHub repository itself -->
+<p align="center">
+<a href><img src="https://img.shields.io/github/last-commit/KorrdsTech/Jeth/DJSv.13/Stable"></a>
+<a href><img src="https://img.shields.io/github/languages/top/KorrdsTech/Jeth"></a>
+<a href="LICENSE"><img src="https://img.shields.io/badge/license-AGPL%20v3-blue.svg"></a>
+</p>
+
+Os servidores do Discord necessitam de uma moderação de qualidade, pensando exatamente nisto desenvolvemos a Jeth, uma bot de extrema qualidade focada neste propósito porém com diversos comandos multipropósito.
+
+Lembre-se nós disponibilizamos nossa SRC para usos a fins estudantis, portanto note que não adianta baixar os arquivos e tentar modifica-los para executar da mesma forma que o bot original, o projeto apresentará possíveis erros desconhecidos por parte do público.
+
+_Precisa de ajuda com algum dos tópicos abaixo?_
+
+## 🤔 Como adiciono a Jeth?
+
+<p align="center">
+<a href="https://top.gg/bot/936773967279173662">
+  <img src="https://top.gg/api/widget/936773967279173662.svg">
+</a>
+
+Caso não esteja conseguindo adicionar o bot por este método ou não goste dele tente adicionar o bot através do próprio discord [clicando aqui](https://discord.com/oauth2/authorize?client_id=936773967279173662&scope=bot+identify+guilds+email+applications.commands&permissions=8) caso apareça algum problema com este link você pode reportar na categoria issues.
+
+## 🐞 Comandos
+Alguns dos comandos criados em nosso projeto hoje se encontram desatualizados e podem apresentar diversas falhas e desagrado visual, caso queira reportar uma destas falhas ou falar sobre o assunto considere entrar em nosso <a href="https://discord.gg/WPUYahyPzx">Discord</a>.
+
+## 💁 Sugestões
+Gostaria de deixar seu feedback sobre nosso projeto? então não pense muito! entre agora em nosso <a href="https://discord.gg/WPUYahyPzx">servidor</a> e deixe sua sugestão ou comentário em relação ao nosso desenvolvimento.
+
+## 💸 Doações
+Para manter a Jeth online necessitamos pagar uma hospedagem para ela, desta forma ela pode ficar online 24 horas todos os 7 dias da semana! entretanto não é um valor baixo que pagamos, caso você sinta que quer coolaborar financeiramente com o projeto deixamos a opção de doação abaixo, a quantia que quer doar é sua escolha e utilizamos o saldo apenas com a Jeth!
+
+[link para as doações](https://donatebot.io/checkout/1001368891160805506)
+
+___
+
+<p align="center">
+<img src="https://i.imgur.com/chexojg.png">
+</p>
---
+++ src/commands/Misc/ProfileCommand.js
@@ -0,0 +1,219 @@
+const { Command } = require('../../utils')
+const { MessageAttachment } = require('discord.js')
+const { loadImage, registerFont, createCanvas } = require('canvas');
+registerFont('src/assets/fonts/Minecraft.ttf', {
+  family: 'Minecraft',
+});
+registerFont('src/assets/fonts/Sugar Snow.ttf', {
+  family: 'Sugar Snow',
+});
+registerFont('src/assets/fonts/my-standard.ttf', {
+  family: 'my-standard',
+});
+registerFont('src/assets/fonts/Montserrat-Black.ttf', { family: 'Montserrat' });
+registerFont('src/assets/fonts/Segoe Print.ttf', { family: 'Segoe Print' });
+registerFont('src/assets/fonts/Segoe UI.ttf', { family: 'Segoe UI' });
+registerFont('src/assets/fonts/Segoe UI Black.ttf', {
+  family: 'Segoe UI Black',
+});
+
+const util = require('../../utils/Util')
+
+const Utils = new util();
+module.exports = class Profile extends Command {
+  constructor(name, client) {
+    super(name, client)
+
+    this.name = 'profile'
+    this.aliases = ['profile', 'userprofile', 'perfil']
+    this.category = 'Misc'
+    this.subcommandsOnly = false
+  }
+
+  async run(message, args) {
+    const jeth = this.client.guilds.cache.get('1047342555685212261')
+    const pUser = message.guild.members.cache.get(args[0]?.replace(/[<@!>]/g, '') || message.author.id)
+    const documento = await this.client.database.guild.getOrCreate(message.guild.id)
+    const prefix = documento.prefix
+    const USER =
+            this.client.users.cache.get(args[0]) ||
+            message.mentions.users.first() ||
+            message.author;
+
+    const user = await this.client.database.user.getOrCreate({ _id: USER.id });
+    const canvas = createCanvas(900, 600);
+    const ctx = canvas.getContext('2d');
+    const member1 = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.author;
+    if (!member1) return message.reply('Algum erro terrível aconteceu, entre em contato com o suporte via /bug')
+    const user1 = await this.client.database.user.getOrCreate(member1.id)
+
+        //========================// Import Avatar //========================//
+
+    const avatar = await loadImage(
+      USER.displayAvatarURL({ format: 'jpeg', size: 2048 })
+    );
+    ctx.drawImage(avatar, 20, 100, 220, 320);
+
+        //========================// Import Background //========================//
+
+    const bg = user.backgroundsActive;
+
+    const backgrounds = {
+      one: {
+        id: 1,
+        background: './src/assets/img/png/Profile_Card_Green.png',
+      },
+      two: {
+        id: 2,
+        background: './src/assets/img/png/Profile_Card_Purple.png',
+      },
+      three: {
+        id: 3,
+        background: './src/assets/img/png/Profile_Card_Different.png',
+      },
+    };
+
+    let back_img = '';
+    if (bg === 0) back_img = './src/assets/img/png/Profile_Card.png';
+    else {
+      back_img = Object.entries(backgrounds).find(([, x]) => x.id === bg)[1]
+        .background;
+    }
+
+    const back = await loadImage(back_img);
+    ctx.drawImage(back, 0, 0, 900, 600);
+
+        //========================// Texts //========================//
+
+        // Username
+
+    ctx.textAlign = 'left';
+    ctx.font = '50px "Segoe UI Black"';
+    ctx.fillStyle = 'rgb(253, 255, 252)';
+    ctx, this.shorten(USER.username, 30), 290, 270;
+    await Utils.renderEmoji(ctx, this.shorten(USER.username, 30), 315, 240);
+        // Badges
+
+    let list = [];
+    const dono_id = message.guild.ownerId;
+    const dono = message.guild.members.cache.get(dono_id);
+    const member = message.guild.members.cache.get(USER.id)
+    if (process.env.OWNERS?.includes(message.author.id)) list.push('CREATOR');
+    if (dono) list.push('SERVER_OWNER');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1047342688086806568')) list.push('STAFF');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1047342688086806568')) list.push('TRUST_SAFETY');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1047342688086806568')) list.push('JETH_PARTNER');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1047342688086806568')) list.push('DEV');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1047342688086806568')) list.push('BUG_HUNTER');
+    if (member && member.premiumSinceTimestamp) list.push('BOOSTER');
+    if (user.vip === true) list.push('VIP');
+    if (user.isMarried) list.push('CASADO');
+    if (jeth.members.cache.get(pUser.user.id)?.roles?.cache?.has('1047342688086806568')) list.push('PATREON');
+
+    const mapper = {
+
+      TRUST_SAFETY: '<:IconCrown:1030872512847945798>',
+      SERVER_OWNER: '<:IconCrown:1030872512847945798>',
+      CREATOR: '<:ThisUserIsCEO:938280523585957978>',
+      HOUSE_BRAVERY: '<:HeroHypeRenwil:1052599369750290484>',
+      HOUSE_BRILLIANCE: '<:HeroHypeUrdim:1030872161902137459>',
+      HOUSE_BALANCE: '<:HeroHypeArame:1030872158940975284>',
+      DEV: '<a:activedeveloperbadgeanimated:1052605173299683411>',
+      BOOSTER: '<:BadgeNitroBoosting_1Month:1030872129731829790>',
+      BUG_HUNTER: '<:BadgeDiscordBugHunterGold:1030872109070680084>',
+      PATREON: '<a:gift4patreon:1057797332881641503>',
+      JETH_PARTNER: '<:ThisUserIsJethPartner:938282680028651600>',
+      STAFF: '<:BadgeDiscordStaff:1030872112841367614>',
+
+    }
+
+    USER.flags.toArray().map(flag => { list.push(mapper[flag] ?? '') } )
+    list = list
+
+      .join(',')
+
+      .replace('CREATOR', '<:ThisUserIsCEO:938280523585957978>')
+      .replace('SERVER_OWNER', '<:IconCrown:1030872512847945798>')
+      .replace('HOUSE_BRAVERY', '<:HeroHypeRenwil:1052599369750290484>')
+      .replace('HOUSE_BRILLIANCE', '<:HeroHypeUrdim:1030872161902137459>')
+      .replace('HOUSE_BALANCE', '<:HeroHypeArame:1030872158940975284>')
+      .replace('DEV', '<a:activedeveloperbadgeanimated:1052605173299683411>')
+      .replace('VIP', '<:Star:1047262776625725460>')
+      .replace('BOOSTER', '<:BadgeNitroBoosting_1Month:1030872129731829790>')
+      .replace('CASADO', '💍')
+      .replace('BUG_HUNTER', '<:BadgeDiscordBugHunterGold:1030872109070680084>')
+      .replace('JETH_PARTNER', '<:ThisUserIsJethPartner:938282680028651600>')
+      .replace('TRUST_SAFETY', '<:Certified_Moderator:1047259863148941373>')
+      .replace('PATREON', '<a:gift4patreon:1057797332881641503>')
+      .replace('STAFF', '<:BadgeDiscordStaff:1030872112841367614>')
+
+    ctx.font = `30px "Minecraft"`;
+
+    await Utils.renderEmoji(ctx, list.split(',').join(' '), 35, 55)
+
+        // Titles
+
+    ctx.textAlign = 'left';
+    ctx.font = '30px "Minecraft"';
+    ctx.fillStyle = 'rgb(253, 255, 252)';
+    ctx.fillText('Coins:', 315, 342);
+    ctx.fillText('XP:', 315, 395);
+    ctx.textAlign = 'center';
+    ctx.font = '20px "Minecraft"';
+    if (user.isMarried) {
+      ctx.font = '20px "Minecraft"';
+      ctx.fillStyle = 'rgb(253, 255, 252)';
+      ctx.fillText('Casado(a) com:', 350, 480);
+      ctx.font = '15px "Minecraft"';
+      ctx.fillText(
+        await this.client.users.fetch(user.userMarried).then((x) => x.tag),
+        350,
+        500
+      );
+    }
+
+        // Reps
+    ctx.textAlign = 'right';
+    ctx.font = '40px "Minecraft"';
+    ctx.fillStyle = 'rgb(253, 255, 252)';
+    ctx.fillText(`${user1.rep}`, 220, 515);
+
+        // Coins/XP
+    ctx.textAlign = 'left';
+    ctx.font = '30px "my-standard"';
+    ctx.fillStyle = 'rgb(253, 255, 252)';
+    ctx.fillText(`${user.coins}`, 415, 342);
+    ctx.fillText(
+      `#${user.level} | ${user.xp}/${(
+        user.nextLevel
+      )}`,
+      280,
+      442
+    );
+
+        // Sobre
+
+    ctx.font = '20px "my-standard"';
+    ctx.fillText(
+      user.about == 'null'
+        ? `Use ${prefix}sobremim para alterar essa mensagem`
+        : user.about.match(/.{1,25}/g).join('\n'),
+      480,
+      365
+    );
+
+        //========================// Create Image //========================//
+
+    const attach = new MessageAttachment(
+      canvas.toBuffer(),`Profile_${USER.tag}_.png`
+    );
+
+    message.reply({ files: [attach] });
+  }
+
+  shorten(text, len) {
+    if (typeof text !== 'string') return '';
+    if (text.length <= len) return text;
+    return text.substr(0, len).trim() + '...';
+  }
+};
